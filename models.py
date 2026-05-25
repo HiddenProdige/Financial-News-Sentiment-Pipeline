a30cb938cf980aac3ad049f46acae2621ef99bf9
HiddenProdige <iwalkalot7@gmail.com>
2026-05-24 22:52:59 -0700

    Add ticker field to Article dataclass

--- models.py
+++ models.py
@@ -11,4 +11,5 @@ class Article:
     author: Optional[str]
     published_at: Optional[str]
     section: Optional[str]
+    ticker: Optional[str] = None
     scraped_at: str = field(default_factory=lambda: datetime.utcnow().isoformat())
